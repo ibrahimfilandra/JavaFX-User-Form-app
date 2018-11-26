5be9f52ce68d7052985ee8ab4f86693210e8c9ae
ifilandra1 <ifilandra1@etf.unsa.ba>
2018-11-26 14:05:09 +0100

    dodan jos jedan label i textfield za unos broja indeksa

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,21 @@
 package sample;
 
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+import java.awt.*;
+
 public class Controller {
+
+    public static TextField imeField;
+    private SimpleStringProperty ime;
+
+    public Controller() { ime=new SimpleStringProperty("");
+    imeField= new TextField();
+    }
+
+
+
 }
